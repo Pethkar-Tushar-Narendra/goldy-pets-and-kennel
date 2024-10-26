837da63f5259598cd6d3f1595d754306fb72efc6
Tushar <pethkartusharnarendra@gmail.com>
2024-10-26 19:57:51 +0530

    Resolved error

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
-import Navbar from "./Navbar"
+// import Navbar from "./Navbar"
 
 export default function Home() {
   const [dogs, setDogs] = useState([])
