e064e14b99760b24a2fcdd8bd18c47c5c63aa04d
Tushar <pethkartusharnarendra@gmail.com>
2024-10-26 19:15:05 +0530

    Done title change

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -51,7 +51,7 @@ export default function Home() {
           <section className="p-8 max-w-7xl mx-auto">
             <div className="text-center">
               <h1 className="pb-4 flex items-center justify-center text-center px-5 text-3xl font-bold lg:text-5xl text-white">
-                The Dog App
+                Goldy Pets And Kennel
               </h1>
 
               <form
